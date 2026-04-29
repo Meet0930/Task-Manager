b3a73256b4d58a841ea37254718fbb8592dc41cc
vatiya kavyakumar <vatiyakavyakumar@vatiyas-MacBook-Air.local>
2026-04-29 17:18:11 +0530

    fix: change SMTP to use SSL on port 465 to bypass firewall

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -133,8 +133,9 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", "forever.clothes11@gmail.com")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", "eoxvqftmtutqgmzm")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
