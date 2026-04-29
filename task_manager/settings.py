dc64a6817b48535e484a3a239db4cb47a3a6d3a2
vatiya kavyakumar <vatiyakavyakumar@vatiyas-MacBook-Air.local>
2026-04-29 16:57:19 +0530

    chore: update email host app password

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -136,5 +136,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", "forever.clothes11@gmail.com")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", "hwawqavkmocpcrbs")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", "eoxvqftmtutqgmzm")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
